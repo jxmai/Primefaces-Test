1ef0b5bbd7f3c6c02b8bcaec35b818f6ce3c8bf5
mai <jason.mai@oltis.ca>
2017-08-16 10:41:55 -0500

    Test Setup for 2638

--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -1,6 +1,9 @@
 package org.primefaces.test;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
@@ -8,12 +11,23 @@ import javax.faces.bean.ViewScoped;
 @ManagedBean(name = "testView")
 @ViewScoped
 public class TestView implements Serializable {
-    
+
+    private static final long serialVersionUID = 4124891683211838799L;
+
     private String testString;
-    
-    @PostConstruct  
+
+    private List<String> testStrings = new ArrayList<String>();
+
+    @PostConstruct
     public void init() {
         testString = "Welcome to PrimeFaces!!!";
+        testStrings.add("1");
+        testStrings.add("2");
+        testStrings.add("3");
+    }
+
+    public void fireActionListener(String str) {
+        System.out.println(str);
     }
 
     public String getTestString() {
@@ -22,5 +36,13 @@ public class TestView implements Serializable {
 
     public void setTestString(String testString) {
         this.testString = testString;
-    }    
+    }
+
+    public List<String> getTestStrings() {
+        return testStrings;
+    }
+
+    public void setTestStrings(List<String> testStrings) {
+        this.testStrings = testStrings;
+    }
 }
