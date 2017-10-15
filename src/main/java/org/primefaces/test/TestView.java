70848b5a3d401867ae553762acffea559db446fe
jxmai <maij@myumanitoba.ca>
2017-10-15 11:48:34 -0500

    2439

--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -1,26 +1,39 @@
 package org.primefaces.test;
 
 import java.io.Serializable;
+
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
+import org.primefaces.model.DefaultOrganigramNode;
+import org.primefaces.model.OrganigramNode;
+
 @ManagedBean(name = "testView")
 @ViewScoped
 public class TestView implements Serializable {
-    
-    private String testString;
-    
-    @PostConstruct  
-    public void init() {
-        testString = "Welcome to PrimeFaces!!!";
-    }
 
-    public String getTestString() {
-        return testString;
-    }
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3587328058256135536L;
 
-    public void setTestString(String testString) {
-        this.testString = testString;
-    }    
+	private String testString;
+
+	@PostConstruct
+	public void init() {
+		testString = "Welcome to PrimeFaces!!!";
+	}
+
+	public String getTestString() {
+		return testString;
+	}
+
+	public void setTestString(String testString) {
+		this.testString = testString;
+	}
+
+	public OrganigramNode getRootNode() {
+		return new DefaultOrganigramNode("root", "TEST", null);
+	}
 }
