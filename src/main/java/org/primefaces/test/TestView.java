622860266801f0c918fcb3f2953899de67c2c90d
jxmai <maij@myumanitoba.ca>
2017-11-07 14:01:15 -0600

    2905

--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -1,26 +1,49 @@
 package org.primefaces.test;
 
 import java.io.Serializable;
-import javax.annotation.PostConstruct;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
 @ManagedBean(name = "testView")
 @ViewScoped
 public class TestView implements Serializable {
-    
-    private String testString;
-    
-    @PostConstruct  
-    public void init() {
-        testString = "Welcome to PrimeFaces!!!";
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 812243260866629419L;
+    private String test = "test";
+    private String test2 = "test2";
+    private String test3 = "test3";
+
+    public void testCommandButton() {
+        System.out.println(test);
+        System.out.println(test2);
+        System.out.println(test3);
     }
 
-    public String getTestString() {
-        return testString;
+    public String getTest() {
+        return test;
     }
 
-    public void setTestString(String testString) {
-        this.testString = testString;
-    }    
+    public void setTest(String test) {
+        this.test = test;
+    }
+
+    public String getTest2() {
+        return test2;
+    }
+
+    public void setTest2(String test2) {
+        this.test2 = test2;
+    }
+
+    public String getTest3() {
+        return test3;
+    }
+
+    public void setTest3(String test3) {
+        this.test3 = test3;
+    }
 }
