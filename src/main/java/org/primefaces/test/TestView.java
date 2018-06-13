1de5c17ba6ecf1e362e0169635e5e364aead247b
jxmai <maij@myumanitoba.ca>
2018-06-12 20:03:24 -0500

    workaround

--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -8,6 +8,10 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.primefaces.event.FileUploadEvent;
 
 @ManagedBean(name = "testView")
@@ -34,8 +38,17 @@ public class TestView implements Serializable {
         FacesContext.getCurrentInstance().addMessage(null, msg);
 
         // skip some steps, just assume we have the hash back
+        Document document = Jsoup.parse(testString);
 
-        testString = testString.concat(fakeHash);
+        Elements elements = document.select("p");
+        if (!elements.isEmpty()) {
+            Element element = elements.first();
+            element.appendText(fakeHash);
+            testString = element.text();
+        } else {
+            testString = testString.concat(fakeHash);
+        }
+
     }
 
     public String getTestString() {
